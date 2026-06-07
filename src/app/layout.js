b96c26b686c7b3b3caca2bf709e7375d25c7242b
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-06-07 21:50:51 +0600

    installed betterauth, base url and secret key is set and created auth instance

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "../components/NavBar";
+
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
