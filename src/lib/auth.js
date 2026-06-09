91914bcebab1390a321cb63e0d62a48423dea695
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-06-09 23:56:35 +0600

    betterauth email password authentication enabled

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,6 +6,9 @@ const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db();
 
 export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
   database: mongodbAdapter(db, {
     client,
   }),
