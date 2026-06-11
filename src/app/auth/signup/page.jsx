ad5897beea41c24e94ea8a7c378aeb1d46b0d239
pritam-b-dev <pritam.bhowmick@ymail.com>
2026-06-12 02:03:52 +0600

    feat: implement user signup with better-auth and mongodb.

---
+++ src/app/auth/signup/page.jsx
@@ -0,0 +1,174 @@
+"use client";
+
+import React, { useState } from "react";
+import {
+  Input,
+  Button,
+  Card,
+  CardHeader,
+  CardBody,
+  Link,
+  Spinner,
+} from "@heroui/react";
+import { Eye, EyeSlash } from "@gravity-ui/icons";
+import { signUp } from "../../../lib/auth-client";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
+// import { authClient } from "@/lib/auth-client";
+
+export default function SignUpPage() {
+  const router = useRouter();
+  const [isVisible, setIsVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const toggleVisibility = () => setIsVisible(!isVisible);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // পাসওয়ার্ড ম্যাচিং চেক
+    if (formData.password !== formData.confirmPassword) {
+      toast.error("Passwords do not match!");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      // Better-Auth এর সাইন-আপ মেথড
+      const { data, error } = await signUp.email({
+        email: formData.email,
+        password: formData.password,
+        name: formData.name,
+      });
+
+      if (error) {
+        console.log(error);
+        toast.error(error.message || "Something went wrong. Try again!");
+        setLoading(false);
+        return;
+      }
+      toast.success("Account created successfully!");
+
+      setTimeout(() => {
+        router.push("/");
+        router.refresh(); // সেশন আপডেট করার জন্য রিফ্রেশ করা
+      }, 2000);
+    } catch (err) {
+      console.log(err);
+      toast.error("Something went wrong. Try again later");
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-background p-4">
+      <Card className="w-full max-w-md p-2 shadow-lg">
+        <div className="flex flex-col items-center gap-1 pb-4 pt-4 text-center">
+          <h1 className="text-2xl font-bold tracking-tight">Create Account</h1>
+          <p className="text-small text-default-500">ENter your information</p>
+        </div>
+
+        <div className="flex flex-col gap-4 pt-2">
+          <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+            <Input
+              required
+              label="Full Name"
+              name="name"
+              placeholder="Enter your name"
+              type="text"
+              variant="bordered"
+              value={formData.name}
+              onChange={handleChange}
+            />
+
+            <Input
+              required
+              label="Email Address"
+              name="email"
+              placeholder="example@mail.com"
+              type="email"
+              variant="bordered"
+              value={formData.email}
+              onChange={handleChange}
+            />
+
+            <div className="relative w-full flex items-center">
+              <Input
+                required
+                label="Password"
+                name="password"
+                placeholder="Enter password"
+                variant="bordered"
+                value={formData.password}
+                onChange={handleChange}
+                type={isVisible ? "text" : "password"}
+                className="w-full"
+              />
+              {/* আইকন বাটনটিকে absolute পজিশন দেওয়া হয়েছে */}
+              <button
+                className="absolute right-3 top-1/2 -translate-y-1/2 focus:outline-none flex items-center justify-center z-10"
+                type="button"
+                onClick={toggleVisibility}
+              >
+                {isVisible ? (
+                  <EyeSlash
+                    size={20}
+                    className="text-zinc-400 pointer-events-none"
+                  />
+                ) : (
+                  <Eye
+                    size={20}
+                    className="text-zinc-400 pointer-events-none"
+                  />
+                )}
+              </button>
+            </div>
+
+            <Input
+              required
+              label="Confirm password"
+              name="confirmPassword"
+              placeholder="confirm password"
+              variant="bordered"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              type={isVisible ? "text" : "password"}
+            />
+
+            <Button
+              isDisabled={loading}
+              color="primary"
+              type="submit"
+              className="w-full font-medium"
+            >
+              {loading ? <Spinner color="white" size="sm" /> : "Sign Up"}
+            </Button>
+
+            <p className="text-center text-small text-default-500 mt-2">
+              Already have an account?{" "}
+              <Link href="/login" size="sm">
+                Sign In Here!
+              </Link>
+            </p>
+          </form>
+        </div>
+      </Card>
+    </div>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
         <NavBar />
         <main>{children}</main>
         <Footer />
+        <Toaster position="top-center" reverseOrder={false} />
       </body>
     </html>
   );
